d661335bfdd5c84c20b36f460c20a0c5ba2e6a3e
Alex R. Ferreira <arf92@live.com>
2016-10-27 21:25:41 -0200

    add de classes e arquivos para test com Junit.

--- Projeto_Eclipse/src.test/main/managepdf/ManagePdfTest.java
+++ Projeto_Eclipse/src.test/main/managepdf/ManagePdfTest.java
@@ -1,12 +1,24 @@
 package main.managepdf;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class ManagePdf {
+import main.manageclip.ManageKindleClip;
+import model.Anotation;
+
+public class ManagePdfTest {
+
+	private ManageAnotationToPDF manageAnotationToPDF;
+	private ManageKindleClip manageKindleClip;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -14,11 +26,28 @@ public class ManagePdf {
 
 	@Before
 	public void setUp() throws Exception {
+		manageAnotationToPDF = new ManageAnotationToPDF();
+		manageKindleClip = new ManageKindleClip();
 	}
 
 	@Test
-	public final void testCreatePDFWithInfo() {
-		fail("Not yet implemented"); // TODO
+	public final void testCreatePDFWithInfo() throws IOException, URISyntaxException {
+		File clipFile = new File(getClass().getResource("/test/managepdf/clip_3_anotations_to_match_pdf.txt").toURI());
+		String pdfNameFile = "pdf_ingles_sem_anotation";
+		String pdfNameFileExt = pdfNameFile.concat(".pdf");
+		File pdfFile = new File(getClass().getResource("/test/managepdf/"+pdfNameFileExt).toURI());
+		
+		List<Anotation> anotations = manageKindleClip.extractInfoFromClipFile(pdfNameFile, clipFile);
+		File createPDFWithInfo = manageAnotationToPDF.createPDFWithInfo(anotations, pdfFile);
+		assertNotNull(createPDFWithInfo);		
+	}
+	
+	/**
+	 * Testar a busca por anotações no PDF
+	 */
+	@Test
+	public final void testAnotationSearching(){
+		
 	}
 
 }
