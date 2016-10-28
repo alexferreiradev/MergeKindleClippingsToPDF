0988ba38f728c5eb76f3dbb345269109291478e9
Alex R. Ferreira <arf92@live.com>
2016-10-27 22:53:50 -0200

    add de novo test. Teste com dados Reais.

--- Projeto_Eclipse/src.test/main/managepdf/ManagePdfTest.java
+++ Projeto_Eclipse/src.test/main/managepdf/ManagePdfTest.java
@@ -1,7 +1,6 @@
 package main.managepdf;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,6 +33,18 @@ public class ManagePdfTest {
 	public final void testCreatePDFWithInfo() throws IOException, URISyntaxException {
 		File clipFile = new File(getClass().getResource("/test/managepdf/clip_3_anotations_to_match_pdf.txt").toURI());
 		String pdfNameFile = "pdf_ingles_sem_anotation";
+		String pdfNameFileExt = pdfNameFile.concat(".pdf");
+		File pdfFile = new File(getClass().getResource("/test/managepdf/"+pdfNameFileExt).toURI());
+		
+		List<Anotation> anotations = manageKindleClip.extractInfoFromClipFile(pdfNameFile, clipFile);
+		File createPDFWithInfo = manageAnotationToPDF.createPDFWithInfo(anotations, pdfFile);
+		assertNotNull(createPDFWithInfo);		
+	}
+	
+	@Test
+	public final void testRealClipAndPDF() throws IOException, URISyntaxException {
+		File clipFile = new File(getClass().getResource("/test/managepdf/My Clippings.txt").toURI());
+		String pdfNameFile = "Piga_comentado";
 		String pdfNameFileExt = pdfNameFile.concat(".pdf");
 		File pdfFile = new File(getClass().getResource("/test/managepdf/"+pdfNameFileExt).toURI());
 		
