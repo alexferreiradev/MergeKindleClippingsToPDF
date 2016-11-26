1618aead96f28168461a6d13303fd22dc864b90d
Alex R. Ferreira <arf92@live.com>
2016-11-25 23:37:38 -0200

    organizacao de src para test seguindo padrao do maven.

---
+++ Projeto_Eclipse/src/test/java/manageclip/ManageClipTest.java
@@ -0,0 +1,79 @@
+package manageclip;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import manageclip.ManageKindleClip;
+import model.Anotation;
+
+public class ManageClipTest {
+
+	private ManageKindleClip manageKindleClip;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		manageKindleClip = new ManageKindleClip();
+	}
+
+	@Test
+	public final void testExtractInfoFromClipFileEmpty() throws Exception {
+		File clipFile = new File(getClass().getResource("/manageclip/clip_empty.txt").toURI());
+		String pdfNameFile = "test";
+		List<Anotation> anotations = manageKindleClip.extractInfoFromClipFile(pdfNameFile, clipFile);
+		assertNotNull(anotations);
+		assertTrue(anotations.size() == 0);
+	}
+	
+	/**
+	 * Sem uma marcação para um nome de PDF
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public final void testExtractInfoFromClipFileWithoutPDFName() throws Exception {
+		File clipFile = new File(getClass().getResource("/manageclip/clip_without_pdf_name.txt").toURI());
+		String pdfNameFile = "test";
+		List<Anotation> anotations = manageKindleClip.extractInfoFromClipFile(pdfNameFile, clipFile);
+		assertNotNull(anotations);
+		assertTrue(anotations.size() == 0);
+	}
+	
+	/**
+	 * PDF: test
+	 * Texto: teste de destaque
+	 * Clip: 3 anotações
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public final void testExtract3Info() throws Exception {
+		File clipFile = new File(getClass().getResource("/manageclip/clip_3_anotations.txt").toURI());
+		String pdfNameFile = "Ludmila";
+		List<Anotation> anotations = manageKindleClip.extractInfoFromClipFile(pdfNameFile, clipFile);
+		assertNotNull(anotations);
+		assertTrue(anotations.size() == 3);
+		Logger.getGlobal().log(Level.ALL, "Anotations size: "+anotations.size());
+		
+		assertTrue(anotations.get(0).getPosition().equals("124"));
+		assertTrue(anotations.get(1).getPosition().equals("125"));
+		assertTrue(!anotations.get(1).getPosition().equals("134"));
+		assertTrue(anotations.get(2).getPosition().equals("126"));
+		assertTrue(anotations.get(0).getText().equals("transaction 1"));
+		assertTrue(anotations.get(1).getText().equals("transaction 2"));
+		assertTrue(anotations.get(2).getText().equals("transaction 3"));
+	}
+
+}
---
+++ Projeto_Eclipse/src/test/java/managepdf/ManagePdfTest.java
@@ -0,0 +1,65 @@
+package managepdf;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import manageclip.ManageKindleClip;
+import managepdf.ManageAnotationToPDF;
+import model.Anotation;
+
+public class ManagePdfTest {
+
+	private ManageAnotationToPDF manageAnotationToPDF;
+	private ManageKindleClip manageKindleClip;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		manageAnotationToPDF = new ManageAnotationToPDF();
+		manageKindleClip = new ManageKindleClip();
+	}
+
+	@Test
+	public final void testCreatePDFWithInfo() throws IOException, URISyntaxException {
+		File clipFile = new File(getClass().getResource("/managepdf/clip_3_anotations_to_match_pdf.txt").toURI());
+		String pdfNameFile = "pdf_ingles_sem_anotation";
+		String pdfNameFileExt = pdfNameFile.concat(".pdf");
+		File pdfFile = new File(getClass().getResource("/managepdf/"+pdfNameFileExt).toURI());
+		
+		List<Anotation> anotations = manageKindleClip.extractInfoFromClipFile(pdfNameFile, clipFile);
+		File createPDFWithInfo = manageAnotationToPDF.createPDFWithInfo(anotations, pdfFile);
+		assertNotNull(createPDFWithInfo);		
+	}
+	
+	@Test
+	public final void testRealClipAndPDF() throws IOException, URISyntaxException {
+		File clipFile = new File(getClass().getResource("/managepdf/My Clippings.txt").toURI());
+		String pdfNameFile = "Piga_comentado";
+		String pdfNameFileExt = pdfNameFile.concat(".pdf");
+		File pdfFile = new File(getClass().getResource("/managepdf/"+pdfNameFileExt).toURI());
+		
+		List<Anotation> anotations = manageKindleClip.extractInfoFromClipFile(pdfNameFile, clipFile);
+		File createPDFWithInfo = manageAnotationToPDF.createPDFWithInfo(anotations, pdfFile);
+		assertNotNull(createPDFWithInfo);		
+	}
+	
+	/**
+	 * Testar a busca por anotações no PDF
+	 */
+	@Test
+	public final void testAnotationSearching(){
+		
+	}
+
+}
